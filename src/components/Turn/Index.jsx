6792f2324f0f76e1bc6f152d992fc38a4204efa7
Jazziel Puente <60794855+jazzielp@users.noreply.github.com>
2024-01-30 12:00:08 -0700

    Fix the Turn component because It was default export

--- src/components/Turn/Index.jsx
+++ src/components/Turn/Index.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './Index.css'
 
-export default function Turn () {
+export function Turn () {
   return (
     <div className='play__turn'>
       <img className='play__turn-img' src='../assets/icon-x-gray.svg' alt='Turn x icon' />

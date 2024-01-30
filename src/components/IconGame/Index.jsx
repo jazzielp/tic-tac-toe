13d7b90d61c59369579a15d93f1445c4307a73de
Jazziel Puente <60794855+jazzielp@users.noreply.github.com>
2024-01-30 10:52:51 -0700

    Change the icon game

--- src/components/IconGame/Index.jsx
+++ src/components/IconGame/Index.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import { Logo } from '../Icons/Icons'
 
 export default function IconGame () {
   return (
     <>
       <picture>
-        <img className='logo' src='../assets/logo.svg' alt='' />
+        <img className='logo' src={Logo} alt='' />
       </picture>
     </>
   )

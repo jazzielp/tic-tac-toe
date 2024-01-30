50768dce3f2e06b4ec3d97ab6cae5157b9073937
Jazziel Puente <60794855+jazzielp@users.noreply.github.com>
2024-01-30 10:52:19 -0700

    Create reset button component

---
+++ src/components/ResetButton/Index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export function ResetButtom () {
+  return (
+    <div class='button-content'>
+      <button class='button'>
+        <img class='icon' src='../assets/icon-restart.svg' alt='Icon restart' />
+      </button>
+    </div>
+  )
+}

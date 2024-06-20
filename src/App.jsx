71e80961d44f5d0d8f81f0bc24585eb9d226c48d
Jazziel Puente <60794855+jazzielp@users.noreply.github.com>
2024-06-20 16:11:50 -0700

    Add a constant to start the dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { useState } from 'react'
-import { TURNS, INITIAL_BOARD } from '../src/const/const'
+import { TURNS, INITIAL_SCORE, INITIAL_BOARD } from '../src/const/const'
 import { Header } from './components/Header/Header'
 import { Board } from './components/Board/Board'
 import { ModalWinner } from './components/ModalWinner/ModalWinner'
 function App () {
   const [board, setBoard] = useState(() => {
     const boardStorage = window.localStorage.getItem('board')
-    return boardStorage ? JSON.parse(boardStorage) : Array(9).fill(null)
+    return boardStorage ? JSON.parse(boardStorage) : INITIAL_BOARD
   })
   const [turn, setTurn] = useState(() => {
     const turnStorage = window.localStorage.getItem('turn')
@@ -15,7 +15,7 @@ function App () {
 
   const [scoreboard, setScoreboard] = useState(() => {
     const scoreboardStorage = window.localStorage.getItem('scoreboard')
-    return scoreboardStorage ? JSON.parse(scoreboardStorage) : INITIAL_BOARD
+    return scoreboardStorage ? JSON.parse(scoreboardStorage) : INITIAL_SCORE
   })
 
   const [comboWinner, setComboWinner] = useState([])
@@ -23,11 +23,20 @@ function App () {
   const [winner, setWinner] = useState(false)
   const [winnerTurn, setWinnerTurn] = useState(null)
 
+  const resetGame = () => {
+    setBoard(INITIAL_BOARD)
+    setTurn(TURNS.X)
+    setScoreboard(INITIAL_SCORE)
+    setComboWinner([])
+    setWinner(false)
+    setWinnerTurn(null)
+  }
+
   return (
     <>
       <main className='main'>
-        {winner && <ModalWinner winnerTurn={winnerTurn} />}
-        <Header turn={turn} />
+        {winner && <ModalWinner winnerTurn={winnerTurn} resetGame={resetGame} />}
+        <Header turn={turn} resetGame={resetGame} />
         <Board
           turn={turn}
           board={board}
